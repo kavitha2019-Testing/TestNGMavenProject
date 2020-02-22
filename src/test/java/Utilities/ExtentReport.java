00b13f97ae893ba3923303cf84c3b092406eb8b1
kavitha <kavitha.abburi@gmail.com>
2020-02-22 00:22:47 +0000

    update Extent Report

--- src/test/java/Utilities/ExtentReport.java
+++ src/test/java/Utilities/ExtentReport.java
@@ -27,7 +27,9 @@ import com.aventstack.extentreports.reporter.configuration.Theme;
         public void onStart(ITestContext testContext)
         {
             String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());//time stamp
-            String repName="Test-Report-"+timeStamp+".html";
+           // String repName="Test-Report-"+timeStamp+".html";
+            String repName="Test-Report.html";
+
 
             htmlReporter=new ExtentHtmlReporter(System.getProperty("user.dir")+ "/Test-output/"+repName);//specify location of the report
             htmlReporter.loadXMLConfig(System.getProperty("user.dir")+ "/Extent-config.xml");
